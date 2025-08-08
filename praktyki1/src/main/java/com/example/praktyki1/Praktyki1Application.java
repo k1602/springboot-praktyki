f816008fc276e4137c01b102248c3e50c22f751b
k1602 <k1602@wp.pl>
2025-08-09 00:32:35 +0200

    a

---
+++ praktyki1/src/main/java/com/example/praktyki1/Praktyki1Application.java
@@ -0,0 +1,13 @@
+package com.example.praktyki1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Praktyki1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Praktyki1Application.class, args);
+	}
+
+}
---
+++ praktyki1/src/test/java/com/example/praktyki1/Praktyki1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praktyki1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praktyki1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
