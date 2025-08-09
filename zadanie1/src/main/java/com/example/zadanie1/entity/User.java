1d0008cb9cda08c9560b2ed888663a216ca18d70
k1602 <k1602@wp.pl>
2025-08-09 23:40:44 +0200

    a

---
+++ zadanie1/src/main/java/com/example/zadanie1/entity/User.java
@@ -0,0 +1,24 @@
+package com.example.zadanie1.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column(nullable = false ,unique = true)
+    private String email;
+
+
+
+}
